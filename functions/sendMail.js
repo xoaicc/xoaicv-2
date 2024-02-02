73bae3808c6c4eb2fefb5d2fdbdfa7f5b0f6d504
Vuong Do <xoaic@xcc.one>
2024-02-03 00:10:13 +0700

    FIX some

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,16 +1,24 @@
 form.onsubmit = (e) => {
     e.preventDefault();
+    const submitBtn = form.querySelector('input[type="submit"]');
 
     if (!grecaptcha.getResponse()) {
-        alert('Vui lòng xác minh captcha!');
+        currentLang === "vi" ? alert("Vui lòng xác minh captcha!") : alert("Please verify captcha!");
         return;
-    } else {
-        emailjs.sendForm('service_xoaicv', 'template_xoaicv', form)
-            .then(res => {
-                alert('SUCCESS!', res.status, res.text);
-            }, err => {
-                alert('FAILED...', err);
-            });
     }
 
-}
+    if (!submitBtn.disabled) {
+        submitBtn.disabled = true;
+
+        emailjs.sendForm("service_xoaicv", "template_xoaicv", form)
+            .then(() => {
+                currentLang === "vi" ? alert("Thư đã được gửi thành công! Hãy kiểm tra hòm thư của bạn") : alert("Email has been sent successfully! Please check your inbox");
+            })
+            .catch(() => {
+                currentLang === "vi" ? alert("The message could not be sent, please check again!") : alert("Không thể gửi được thư, vui lòng kiểm tra lại!");
+            })
+            .finally(() => {
+                submitBtn.disabled = false;
+            });
+    }
+};
